cf21b14cd93ad2ac5ec3fc0309e4c6c6d66ede56
unknown <duongducchinh160@gmail.com>
2022-08-01 14:31:58 +0700

    cricle vs Cylinder

---
+++ src/Rikei/Academy/Main.java
@@ -0,0 +1,11 @@
+package Rikei.Academy;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5,"blue");
+        System.out.println("circle = "+circle);
+        Cylinder cylinder = new Cylinder(5 ,"blue",15);
+        System.out.println("cylinder = "+cylinder);
+
+    }
+}
